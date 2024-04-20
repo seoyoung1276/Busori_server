148601b856ed7cca8577fbfa845a8b50f155389a
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2024-04-20 18:21:48 +0900

    init

---
+++ models/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+
+
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
