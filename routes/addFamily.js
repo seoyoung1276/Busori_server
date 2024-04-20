f8296dc6708a0c42d74d242c683a55a9147da277
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2024-04-20 23:03:58 +0900

    친구 요청 보내기 추가

---
+++ routes/addFamily.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const User = require('../models/User')
+const Family = require('../models/Family')
+const router = express.Router()
+
+router.post('/sendfollow', async (req, res) => {
+    try{
+        const { invite_code } = req.body;
+        const findInviteCode = await User.findOne({ where: {invite_code}})
+        
+        if(findInviteCode){
+            const currentUser = req.session.user;
+
+            const newFamily = await Family.create({
+                following_no : findInviteCode.user_no,
+                follower_no : currentUser.user_no
+            })
+            return res.status(201).json(newFamily)
+        }else{
+            console.log(error)
+            return res.status(404).json({ error : 'Wrong Invite Code'})
+        }
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({ error : 'send follow error'})
+    }
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const auth = require('./auth')
+const addFamily = require('./addFamily')
 
 const router = express.Router();
 
 router.use(auth)
+router.use(addFamily)
 
 module.exports = router;
